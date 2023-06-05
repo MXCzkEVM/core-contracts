2575969c11c8602cd30c098faf19f1913106a021
luanxu-mxc <luanxu@mxc.org>
2023-06-05 23:00:23 +0800

    feat: generate_transactionL2.ts

--- tasks/generate_transactionL2.ts
+++ tasks/generate_transactionL2.ts
@@ -1,6 +1,7 @@
 import { task } from "hardhat/config";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {getNamedSigners} from "hardhat-deploy-ethers/internal/helpers";
+import {sleep} from "./utils";
 
 task("generate_transaction_L2").setAction(async (args, hre: any) => {
   await generateTransactionL2(hre);
@@ -14,7 +15,7 @@ export async function generateTransactionL2(hre: HardhatRuntimeEnvironment) {
         to: "0x52f60448790E485F38f2Aa9c867CD0DD647c0b73",
         value: 1,
       });
-
+      await sleep(500);
     } catch (e) {
       console.log(e);
     }
